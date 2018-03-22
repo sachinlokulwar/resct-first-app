e9012f8691bbdddf6bd2cd35bb1a8cf305ded07f
Sachin Lokulwar <GS-0997@GSLAB.COM>
2018-03-22 10:59:22 +0530

    state handling between two component

--- src/message.js
+++ src/message.js
@@ -12,7 +12,11 @@ export default class Message extends Component {
           <div className="row row-no-margin">
             <div className={"message-container col-md-9 " + (this.props.currentUser ==  this.props.message.sender_id? 'offset-md-3 right' : 'left')}>
                 <div className="message">
-                    {this.props.message.message}
+                    <p className="message-header">
+                        <span className="sender pull-left">{this.props.message.sender ? this.props.message.sender : 'Me'}</span>
+                        <span className="sent-at pull-right">{this.props.message.time.getDate() + '-' + (this.props.message.time.getMonth() + 1) + ' ' + this.props.message.time.getHours() + ':' + this.props.message.time.getMinutes()}</span>
+                    </p>
+                    <p className="message-body">{this.props.message.message}</p>
                 </div>
             </div>
           </div>
